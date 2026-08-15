c80da5260dd87dd1669c6d489167171e26e8e9ad
kvxd <redslyice@outlook.de>
2026-08-15 22:04:52 +0200

    feat: finish the sm83
    phase timing is not machine-cycle accurate!!!

--- examples/gameboy/bundle-sm83.sh
+++ examples/gameboy/bundle-sm83.sh
@@ -5,6 +5,8 @@ output=${1:?}
 awk 'FNR == 1 && NR != 1 { print "" } { print }' \
     "$gameboy_dir/sm83-register-file.dust" \
     "$gameboy_dir/sm83-alu.dust" \
+    "$gameboy_dir/sm83-misc.dust" \
+    "$gameboy_dir/sm83-cb.dust" \
     "$gameboy_dir/sm83-address-state.dust" \
     "$gameboy_dir/sm83-fetch-control.dust" \
     "$gameboy_dir/sm83-ram.dust" \
