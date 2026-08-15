d51628a1d0439f2782b155bc5463bed44e4e1bd5
kvxd <redslyice@outlook.de>
2026-08-15 20:30:23 +0200

    feat: 8bit-alu, PC, SP, instruction register, address path, controller, cpu state

---
+++ examples/gameboy/bundle-sm83.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+gameboy_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+output=${1:?}
+awk 'FNR == 1 && NR != 1 { print "" } { print }' \
+    "$gameboy_dir/sm83-register-file.dust" \
+    "$gameboy_dir/sm83-alu.dust" \
+    "$gameboy_dir/sm83-address-state.dust" \
+    "$gameboy_dir/sm83-fetch-control.dust" \
+    "$gameboy_dir/sm83-ram.dust" \
+    "$gameboy_dir/sm83-core.dust" > "$output"
